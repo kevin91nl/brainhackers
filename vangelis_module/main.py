ca2d867572704eec35b37d776b9d8dceb44f33d4
Vaggelis <underworldseed@gmail.com>
2017-01-23 21:43:51 +0100

    Testing a weird thing.

--- vangelis_module/main.py
+++ vangelis_module/main.py
@@ -26,6 +26,16 @@ model = Sequential()
 
 model.add(Embedding(vocab_size, embedding_dims,
                     dropout=.0,trainable=True))
+
+
+# model.add(Convolution1D(nb_filter=64,
+#                             filter_length=5,
+#                             border_mode='valid',
+#                             activation='relu',
+#                             subsample_length=1))
+# model.add(MaxPooling1D(pool_length=4))
+
+
 model.add(LSTM(128, dropout_W=.2, dropout_U=.2, return_sequences=True))
 model.add(TimeDistributed(Dense(4)))
 model.add(Activation('sigmoid'))
@@ -38,22 +48,24 @@ model.compile(loss='binary_crossentropy',
 
 
 
-
+model.summary()
 #prepare data
 data_x=np.zeros((1620,20))
 for batch in range(corpus.X.shape[0]):
     data_x[batch,:]=processor.kmeans.predict(corpus.X[batch,:,:])
 
-model.fit(data_x, corpus.Y, batch_size=32, nb_epoch=2, validation_split=.2)
+#1000 for training
+model.fit(data_x[:1000], corpus.Y[:1000], batch_size=32, nb_epoch=2, validation_split=.0)
 
 y_output = model.predict(data_x)
 y_output[np.where(y_output<0.5)]=0
 y_output[np.where(y_output>=0.5)]=1
 
+#620 for testing
 accuracy=0
-for i in range(1620):
+for i in range(1000,1620):
     for j in range(20):
         accuracy += accuracy_score(y_output[i,j], corpus.Y[i,j])
-print("Mean Accuracy: %f"%(accuracy/(1620*20)))
+print("Mean Accuracy: %f"%(accuracy/(620*20)))
 
 
