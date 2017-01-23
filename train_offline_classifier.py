edd943609d7bd0ec8676df16eb6db2eddbb60f89
Kevin Jacobs <kevin91nl@gmail.com>
2017-01-23 14:14:50 +0100

    Added preprocessing notes.

--- train_offline_classifier.py
+++ train_offline_classifier.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 
 import os, sys, random, math, time, socket, struct
+
+import scipy.signal
+
 bufferpath = "../../python/signalProc"
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),bufferpath))
 import bufhelp
@@ -10,7 +13,11 @@ import bufhelp
 with open('Merian.pickle', 'rb') as file:
     data = pickle.load(file)
 
-print(data['events'][0])
-df = pd.DataFrame(np.asmatrix(data['data'][0]))
-for i in range(37):
-    print(df[i].describe())
+import matplotlib.pyplot as plt
+m = np.asmatrix(data['data'][20])
+m = m[:, 0:9]
+
+m = scipy.signal.detrend(m, axis=1)
+
+plt.plot(m)
+plt.savefig('test.png')
